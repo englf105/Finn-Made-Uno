79e5c21bb24b08b25c3ff68ab39512c19ac8c679
englf105 <englf105@hsd.k12.or.us>
2026-02-03 13:01:06 -0800

    polished code 4

--- src/finn_made_uno/finn_made_uno.py
+++ src/finn_made_uno/finn_made_uno.py
@@ -5,29 +5,23 @@ by: Finn English
 
 
 from game import Game
-from player import Player
-from player_ai import Ai
 
 
 def main():
 
-    # Game setup
+    """ Game setup """
     uno = Game()
     players = []
     uno.addPlayers(players, uno)
     least_cards = len(players[uno.turn].hand.cards)
 
-    # Game Loop
+    """ Game Loop """
     while len(players[uno.turn].hand.cards) != 0:
-
         uno.displayTurnInfo(players)
-        
         uno.playerTurn(players, uno)
-
         if least_cards > len(players[uno.turn].hand.cards):
             least_cards = len(players[uno.turn].hand.cards)
         if least_cards == 0: break
-
         uno.nextTurn()
 
     print(f"\n\033[34m===== Player {uno.turn + 1} won Uno! =====\033[0m\n")
