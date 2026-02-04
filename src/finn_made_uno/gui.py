61bbab60a46d5211dee2dfa0b256f57283bdf4f3
englf105 <englf105@hsd.k12.or.us>
2026-02-04 10:15:37 -0800

    working on gui with tkinter

--- src/finn_made_uno/gui.py
+++ src/finn_made_uno/gui.py
@@ -1,38 +1,10 @@
-import sys
-from PySide6.QtWidgets import (
-    QApplication,
-    QLabel,
-    QMainWindow,
-    QVBoxLayout,
-    QWidget,
-)
+import tkinter as tk
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
+root = tk.Tk()
+root.title("Finn Made Uno")
+file_path = "C:/Users/englf105/Documents/Github Projects/Finn-Made-Uno/"
+file_path += "Finn-Made-Uno/src/finn_made_uno/assets/uno_icon.png"
+icon_image = tk.PhotoImage(file=file_path)
+root.iconphoto(False, icon_image)
 
-        self.setWindowTitle("Window Title")
-        self.setContentsMargins(12, 12, 12, 12)
-        self.resize(320, 240)
-
-        layout = QVBoxLayout()
-        title_label = QLabel("Title Label (Make this bigger, please!)")
-
-        # add widgets & layouts to main layout
-        layout.addWidget(title_label)
-
-        # [OPTIONAL] Add a stretch to move everything up
-        layout.addStretch()
-
-        widget = QWidget()
-        widget.setLayout(layout)
-
-        # Set the central widget of the Window.
-        self.setCentralWidget(widget)
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-
-    app.exec()
+root.mainloop()
