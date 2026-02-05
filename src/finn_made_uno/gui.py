44337975174de969615f4d70f43e731549a38187
Finn T. English <fenglish07@gmail.com>
2026-02-04 18:07:55 -0800

    completed game

--- src/finn_made_uno/gui.py
+++ src/finn_made_uno/gui.py
@@ -2,9 +2,10 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title("Finn Made Uno")
-file_path = "C:/Users/englf105/Documents/Github Projects/Finn-Made-Uno/"
-file_path += "Finn-Made-Uno/src/finn_made_uno/assets/uno_icon.png"
-icon_image = tk.PhotoImage(file=file_path)
-root.iconphoto(False, icon_image)
+root.geometry("800x600")
+
+mini_icon = "C:/Users/Fengl/OneDrive/documents/Github Projects/Finn-Made-Uno/src/finn_made_uno/assets/uno_icon_32.png"
+icon_image_small = tk.PhotoImage(file=mini_icon)
+root.iconphoto(False, icon_image_small)
 
 root.mainloop()
