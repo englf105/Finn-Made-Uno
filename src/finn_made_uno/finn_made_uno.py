8e302b2a12764bf132b673b18d151fcef11c92ad
englf105 <englf105@hsd.k12.or.us>
2026-03-09 09:28:26 -0700

    little change to code

--- src/finn_made_uno/finn_made_uno.py
+++ src/finn_made_uno/finn_made_uno.py
@@ -15,7 +15,7 @@ def main():
     """ Settings """
     uno.place_after_draw = False # Working
     uno.draw_till_place = False # Working
-    uno.stack_plus_cards = True # Working
+    uno.stack_plus_cards = False # Working
 
     """ Game Loop """
     while uno.playerHasCards():
